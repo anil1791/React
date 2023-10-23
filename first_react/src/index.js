df190544094d61e70cd124af294565f43163de47
anil1791 <1234anil2@gmail.com>
2023-10-24 01:41:48 +0530

    added add and subtract buttons

--- first_react/src/index.js
+++ first_react/src/index.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+//const singleElement = (<a href="www.google.com" target="_blank">click to go to Google</a>)
+/*
+const reactElement = React.createElement(
+  "a",
+  { href: "https://www.google.com", target: "_blank" },
+  "Click this to go to Google",
+  evaluated_expressions
 );
+*/
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+
+    <App/>
+    
+  
+);
